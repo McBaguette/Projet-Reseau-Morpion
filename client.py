e2d85a779373fb37ca33fcfde4e9fc088ad2502f
Valentin Gaillard <valentin.gaillard@etu.u-bordeaux.fr>
2017-01-06 19:50:22 +0100

    ajout implémentation server.py et client.py

--- client.py
+++ client.py
@@ -3,16 +3,103 @@ import socket
 import select
 import threading
 
+START_GAME = 5
+ERROR_MOVE = 6
+GAME_MOVE = 7
+ACK = 8
+GAME_OVER = 9
+GOOD_CASE = 10
+SPECTATOR = 11
+WINNER = 12
+LOOSER = 13
+
 hote = "localhost"
-port = 7777
+port = 7778
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((hote, port))hote = "localhost"
+array_player = [2,2,2,2,2,2,2,2,2]
+array_game = [2,2,2,2,2,2,2,2,2]
+game_over = 0
 
-def print:
+connexion_with_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #le client se connecte au serveur
+connexion_with_server.connect((hote, port))
+print("Connexion établie avec le serveur sur le port {}".format(port))
+
+def print_help():
+    print("         0|1|2")
+    print("         3|4|5")
+    print("         6|7|8")                                    
+    print("")
     
-while(1):
+def print_game_player():#affiche le tableau de jeu du joueur
+
+    for i in range(len(array_player)):
+        if i % 3 == 0:
+            print("")
+        if (array_player[i] == 2):
+            print(" ", end=" | ")
+        else:
+            print("U", end=" | ")
+    print("")
+    print("")
     
-    if(
+def print_game():#affiche le tableau de jeu du joueur
     
-socket.close()
+    for i in range(len(array_game)):
+        if i % 3 == 0:
+            print("")
+        if (array_game[i] == 2):
+            print(" ", end=" | ")
+        elif (array_game[i] == 0):
+            print("O", end=" | ")
+        else:
+            print("X", end=" | ")
+    print("")
+    print("")
+    
+while(game_over!=1):
+        
+    response=connexion_with_server.recv(1024).decode(); #reçoit une réponse du serveur
+    response=int(response)
+  
+    if(response == START_GAME):
+        
+        print("Le nombre de joueur est atteint !")
+        print_help()
+                
+    if(response==GAME_MOVE or response==ERROR_MOVE): #si on doit choisir une case pour jouer ou si la case donnée précedemment est fausse
+                   
+        if(response==ERROR_MOVE):
+            print("Le mouvement est impossible")
+        move=0
+        while move!=1:
+            message=input("Entrer une case : ")
+            message=int(message)
+            if((message<10)&(message>=0)): #vérifie si la case est comprise dans l'intervalle du tableau de jeu
+                message = str(message).encode()
+                connexion_with_server.send(message)
+                move=1
+            else:
+                message=print("Erreur de frappe")
+
+    if(response==GAME_OVER or response==SPECTATOR): #affiche le tableau du jeu reçu par le serveur
+        if(response==SPECTATOR):
+            print("O = J1 | X = J2")
+        array_game = connexion_with_server.recv(1024)
+        print_game()
+
+        
+    if(response==GOOD_CASE): #si la case jouait précédemment était valide alors on affiche le tableau du jeu du joueur
+        message=int(message)
+        array_player[message]=3
+        print_game_player()
+                
+    if(response==WINNER):
+        print("Vous avez gagné")
+        game_over=1
+
+    if(response==LOOSER):
+        print("Vous avez perdu")
+        game_over=1
+        
+connexion_with_server.close()
+
--- serveur.py
+++ serveur.py
@@ -1,61 +1,152 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import socket
 import select
 import threading
+import time
 
-nb_client= 0
-J1=NULL
-J2=NULL
+START_GAME = 5
+ERROR_MOVE = 6
+GAME_MOVE = 7
+ACK = 8
+GAME_OVER = 9
+GOOD_CASE = 10
+SPECTATOR = 11
+WINNER = 12
+LOOSER = 13
+
+hote= "localhost"
+port = 7778
+
+nb_client = 0
+gamers=[]
 spectator = []
-array_game = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+array_game = [2,2,2,2,2,2,2,2,2]
+current_player = 0
+game_over=0
 
 def winner(player):
-    assert(player==J1 or player==J2)
+    if (player==gamers[0]): #on attribue la valeur de la case de chaque joueur
+        player_box=0
+    if (player==gamers[1]):
+        player_box=1
+
     # horizontal line
     for y in range(3): 
-        if cells[y*3] == player and cells[y*3+1] == player and cells[y*3+2] == player:
+        if array_game[y*3] == player_box and array_game[y*3+1] == player_box and array_game[y*3+2] == player_box:
             return True
-    # vertical line
-    for x in range(3): 
-        if cells[x] == player and cells[3+x] == player and cells[6+x] == player:
+    #vertical line
+    for x in range(3):
+        if array_game[x] == player_box and array_game[3+x] == player_box and array_game[6+x] == player_box:
             return True
     #diagonals :
-    if cells[0] == player and cells[4] == player and cells[8] == player:
+    if array_game[0] == player_box and array_game[4] == player_box and array_game[8] == player_box:
         return True
-    if cells[2] == player and cells[4] == player and cells[6] == player:
+    if array_game[2] == player_box and array_game[4] == player_box and array_game[6] == player_box:
         return True
+        
     return False
-
+	
 def gameOver():
-    if winner(J1):
-        return J1
-    if winner(J2):
-        return J2
-    for i in range(NB_CELLS):
-        if(cells[i]== EMPTY):
-            return -1
-    return 0
+    global game_over
+    if winner(gamers[0]):
+        game_over=1
+        return gamers[0]    
+    if winner(gamers[1]):
+        game_over=1
+        return gamers[1]
+    return 0         
 
 def game():
-    while 1:
-        if(nb_client>1):
-            sendto(data[, flags], J2)
-            
+    
+    while nb_client<2: #on attend deux joueurs pour commencer la partie
+        time.sleep(1)
         
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(('', 7777))
-connexion_principale.listen(5)
-threadgame = threading.Thread(None, game,None, (), {})
+    message = str(START_GAME).encode()#envoie du signal de début de jeu
+    gamers[0].send(message)
+    gamers[1].send(message)
+
+    current_player = gamers[1]#le premier joueur est le dernier joueur a avoir rejoint la connexion
+    
+    global game_over
+    
+    while (game_over != 1):#tant qu'aucun des deux joueurs a gagné on continue
+    
+        message = str(GAME_MOVE).encode() #envoie de message pour demander au joueur actif de choisir une case
+        time.sleep(0.1)
+        current_player.send(message)
+        time.sleep(0.1)
+            
+        position = current_player.recv(1024).decode() #on attends la réponse du joueur actif
+        position = int(position)
+                
+        for i in range(len(array_game)): #on vérifie chaque position pour savoir si elle est valide ou non
+            if i==position:
+                if array_game[i] == 2:
+                    if(current_player==gamers[0]):
+                        array_game[i]=0
+                        last_player=gamers[0]
+                        message=str(GOOD_CASE).encode()
+                        gamers[0].send(message)
+                        time.sleep(0.1)
+                    else:
+                        array_game[i]=1
+                        last_player=gamers[1]
+                        message=str(GOOD_CASE).encode()
+                        gamers[1].send(message)
+                        time.sleep(0.1)
+                else:
+                    message=str(ERROR_MOVE).encode()
+                    current_player.send(message)
+                    time.sleep(0.1)
+                                                
+        if (last_player == gamers[0]): #on change le joueur actif si son coup était valide
+            current_player=gamers[1]
+        else:
+            current_player=gamers[0]
+
+        gameOver() #on vérifie si la partie est terminée
+        
+    message = str(GAME_OVER).encode() #on envoie un message pour dire que la partie est terminé
+    gamers[0].send(message)
+    gamers[1].send(message)
+    message = str(SPECTATOR).encode()
+    spectator[0].send(message)
+    
+    time.sleep(1)
+    gamers[0].send(bytes(array_game)) #on envoie le tableau final du jeu
+    gamers[1].send(bytes(array_game))
+    spectator[0].send(bytes(array_game))
+    time.sleep(1)
+    
+    winner=gameOver()
+    
+    if(winner==gamers[0]): #on envoie le résultat de la partie au joueur
+        message = str(WINNER).encode()
+        gamers[0].send(message)
+        message = str(LOOSER).encode()
+        gamers[1].send(message)
+    else:
+        message = str(WINNER).encode()
+        gamers[1].send(message)
+        message = str(LOOSER).encode()
+        gamers[0].send(message)
+    
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #on ouvre le serveur
+socket.bind(('', port))
+socket.listen(5)
+print("Démarrage serveur : port : {}".format(port))
+
+threadgame = threading.Thread(None, game,None, (), {}) #on crée puis on démarre une thread qui va gérer la partie
 threadgame.start()
 
-while gameOver():
+while 1: #on sauvegarde les connexions
     client, address = socket.accept()
     nb_client = nb_client+1
     if(nb_client==1):
-        J1=client
+        gamers.append(client)
     if(nb_client==2):
-        J2=client
+        gamers.append(client)
     if(nb_client>2):
         spectator.append(client)
 
-socket.close()
